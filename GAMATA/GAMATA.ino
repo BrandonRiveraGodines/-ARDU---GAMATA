7278227b455ef1b91936d7516dba5c9fd817c4c2
BrandonRiveraGodines <brandonriveragodines@hotmail.com>
2017-09-18 18:55:52 -0500

    18-09-2017 > 06:55
    Agregamos los pines necesarios para el motor.

--- GAMATA/GAMATA.ino
+++ GAMATA/GAMATA.ino
@@ -1,3 +1,12 @@
+#define IN1  13   // Pines para M1
+#define IN2  12   // Pines para M1
+#define IN3  11   // Pines para M1
+#define IN4  10   // Pines para M1
+#define ON1 9     // Pines para M2
+#define ON2 8     // Pines para M2
+#define ON3 7     // Pines para M2
+#define ON4 6     // Pines para M2
+
 void setup() {
   // put your setup code here, to run once:
 
