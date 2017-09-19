290198108a19e334f769a37db81e9597039fac6a
BrandonRiveraGodines <brandonriveragodines@hotmail.com>
2017-09-18 19:00:06 -0500

    18-09-2017 - 07:00
    Agrega el SETUP necesario para los motores.

--- GAMATA/GAMATA.ino
+++ GAMATA/GAMATA.ino
@@ -7,8 +7,40 @@
 #define ON3 7     // Pines para M2
 #define ON4 6     // Pines para M2
 
+// Variables necesarias para el motor.
+int steps_left=4095;
+boolean Direction = true;
+boolean Direction2 = true;
+int Steps = 0;
+int Steps2 = 0;
+int a = 0;
+int Paso [ 8 ][ 4 ] ={   
+    {1, 0, 0, 0},
+    {1, 1, 0, 0},
+    {0, 1, 0, 0},
+    {0, 1, 1, 0},
+    {0, 0, 1, 0},
+    {0, 0, 1, 1},
+    {0, 0, 0, 1},
+    {1, 0, 0, 1}
+};
+
+
+
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+  // Setup necesario para los motores
+  Serial.begin(9600);
+  pinMode(IN1, OUTPUT); 
+  pinMode(IN2, OUTPUT); 
+  pinMode(IN3, OUTPUT); 
+  pinMode(IN4, OUTPUT); 
+  pinMode(ON1, OUTPUT); 
+  pinMode(ON2, OUTPUT); 
+  pinMode(ON3, OUTPUT); 
+  pinMode(ON4, OUTPUT);
 
 }
 
